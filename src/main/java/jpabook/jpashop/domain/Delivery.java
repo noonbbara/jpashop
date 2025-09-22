7886a9eb0e303efe275c2c3429ae20927145616c
DESKTOP-N0Q4ELF\ys <ysysys1226@gmail.com>
2025-09-22 22:15:03 +0900

    13

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -11,6 +11,7 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 
     @Embedded
